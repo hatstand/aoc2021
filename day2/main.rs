10d3a971c440582d754b2aa9f8b9a1833418b0f3
John Maguire <john.maguire@gmail.com>
2021-12-12 22:04:35 +0000

    Day 2 part 1

---
+++ day2/main.rs
@@ -0,0 +1,45 @@
+use itertools::Itertools;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+use regex::Regex;
+
+fn main() {
+    let re = Regex::new(r"(\w+)\s+(\d+)").unwrap();
+
+    if let Ok(lines) = read_lines("./day2/input.txt") {
+        let instructions: Vec<(_, _)> = lines
+            .filter_map(|l| l.ok())
+            .filter_map(|l| {
+                let caps = re.captures(&l);
+                caps.map(|c| (c[1].to_string(), c[2].parse::<u64>().unwrap()))
+            })
+            .collect();
+
+        println!("{} instructions", instructions.len());
+
+        let loc = instructions.iter().fold((0, 0), |(horizontal, depth), (instruction, amount)| {
+            println!("processing: {:?}", (instruction, amount));
+
+            match instruction.as_str() {
+                "forward" => (horizontal + amount, depth),
+                "down" => (horizontal, depth + amount),
+                "up" => (horizontal, depth - amount),
+                _ => (horizontal, depth),
+            }
+        });
+
+        println!("{:?} ({})", loc, loc.0 * loc.1);
+    }
+}
+
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
