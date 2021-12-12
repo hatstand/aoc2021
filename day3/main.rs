ad4db7ab3dadc6ed419001860cd000a8cd282f11
John Maguire <john.maguire@gmail.com>
2021-12-12 22:33:27 +0000

    Day 3 part 1

--- day3/main.rs
+++ day3/main.rs
@@ -7,23 +7,25 @@ use std::path::Path;
 fn main() {
     println!("Hello, world!");
 
-    if let Ok(lines) = read_lines("./day3/example.txt") {
+    if let Ok(lines) = read_lines("./day3/input.txt") {
         let diagnostics: Vec<String> = lines
             .filter_map(|l| l.ok())
-            .filter(|s| s.len() == 5)
             .collect();
+
+        let v2: Vec<&str> = diagnostics.iter().map(|s| &**s).collect();
 
         println!("valid lines: {}", diagnostics.len());
 
-        let g = gamma(diagnostics);
+        let g = gamma(&diagnostics);
+        let e = epsilon(&diagnostics);
 
-        println!("{}", g);
+        println!("{}/{} => {}", g, e, g*e);
     }
 }
 
-fn gamma(input: Vec<String>) -> u8 {
-    let mut gamma: u8 = 0;
-    for i in 0..5 {
+fn gamma(input: &Vec<String>) -> u64 {
+    let mut gamma: u64 = 0;
+    for i in 0..12 {
         let (ones, zeroes) = input.iter()
             .map(|s| s.chars().nth(i).unwrap())
             .fold((0, 0), |(ones, zeros), c| {
@@ -35,10 +37,30 @@ fn gamma(input: Vec<String>) -> u8 {
             });
         let bit = if ones > zeroes { 1 } else { 0 };
 
-        gamma = gamma | (bit << 4-i);
+        gamma = gamma | (bit << 11-i);
     }
 
     gamma
+}
+
+fn epsilon(input: &Vec<String>) -> u64 {
+    let mut epsilon: u64 = 0;
+    for i in 0..12 {
+        let (ones, zeroes) = input.iter()
+            .map(|s| s.chars().nth(i).unwrap())
+            .fold((0, 0), |(ones, zeros), c| {
+                match c {
+                    '0' => (ones, zeros + 1),
+                    '1' => (ones + 1, zeros),
+                    _ => panic!("invalid input"),
+                }
+            });
+        let bit = if ones > zeroes { 0 } else { 1 };
+
+        epsilon = epsilon | (bit << 11-i);
+    }
+
+    epsilon
 }
 
 // The output is wrapped in a Result to allow matching on errors
