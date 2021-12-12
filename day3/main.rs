a95322a177f405540082952c178bf68b9612bc36
John Maguire <john.maguire@gmail.com>
2021-12-12 22:25:55 +0000

    Day 3 example

---
+++ day3/main.rs
@@ -0,0 +1,52 @@
+use itertools::Itertools;
+use itertools::multizip;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() {
+    println!("Hello, world!");
+
+    if let Ok(lines) = read_lines("./day3/example.txt") {
+        let diagnostics: Vec<String> = lines
+            .filter_map(|l| l.ok())
+            .filter(|s| s.len() == 5)
+            .collect();
+
+        println!("valid lines: {}", diagnostics.len());
+
+        let g = gamma(diagnostics);
+
+        println!("{}", g);
+    }
+}
+
+fn gamma(input: Vec<String>) -> u8 {
+    let mut gamma: u8 = 0;
+    for i in 0..5 {
+        let (ones, zeroes) = input.iter()
+            .map(|s| s.chars().nth(i).unwrap())
+            .fold((0, 0), |(ones, zeros), c| {
+                match c {
+                    '0' => (ones, zeros + 1),
+                    '1' => (ones + 1, zeros),
+                    _ => panic!("invalid input"),
+                }
+            });
+        let bit = if ones > zeroes { 1 } else { 0 };
+
+        gamma = gamma | (bit << 4-i);
+    }
+
+    gamma
+}
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
