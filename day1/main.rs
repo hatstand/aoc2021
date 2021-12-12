1cc9a0e8cc43633e7a4efb309f96929264afb204
John Maguire <john.maguire@gmail.com>
2021-12-12 21:45:35 +0000

    Day 1 of Advent of Code

---
+++ day1/main.rs
@@ -0,0 +1,35 @@
+use itertools::Itertools;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() {
+    println!("Hello, world!");
+
+    if let Ok(lines) = read_lines("./day1/input.txt") {
+        let depths: Vec<u64> = lines
+            .filter_map(|l| l.ok())
+            .filter_map(|l| l.parse().ok())
+            .collect();
+
+        println!("valid lines: {}", depths.len());
+
+        let count = depths.iter().tuple_windows::<(_, _)>().fold(0, |acc, (a, b)| {
+            let increased = b > a;
+            println!("{} {} ({})", a, b, if increased { "increased" } else { "n/a" });
+            return if increased { acc + 1 } else { acc };
+        });
+
+        println!("{}", count);
+    }
+}
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
