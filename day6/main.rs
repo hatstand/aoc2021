9367b0b3d8b9ad8fb0bf45b63dbd0456cc167125
John Maguire <john.maguire@gmail.com>
2021-12-13 02:20:55 +0000

    Day6 part 2

--- day6/main.rs
+++ day6/main.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
@@ -9,44 +8,30 @@ fn main() {
 
         let fish: Vec<u8> = line.split(",").map(|x| x.parse().unwrap()).collect();
 
+        let mut fishies: Vec<u64> = vec![0; 9];
+        fish.iter().for_each(|x| {
+            fishies[*x as usize] += 1;
+        });
+
         // Simulate each _type_ of fish and build a lookup table.
-        let types = vec![0, 1, 2, 3, 4, 5, 6, 7, 8];
-        let n = 80;
-        let m: HashMap<_, _> = types
-            .iter()
-            .map(|t| {
-                let start = vec![*t];
+        let n = 256;
 
-                let end_result = (0..n).fold(start, |previous, _| {
-                    let next: Vec<_> = previous
-                        .iter()
-                        .flat_map(|f| {
-                            let next: Vec<_> = match f {
-                                0 => vec![6, 8],
-                                1 => vec![0],
-                                2 => vec![1],
-                                3 => vec![2],
-                                4 => vec![3],
-                                5 => vec![4],
-                                6 => vec![5],
-                                7 => vec![6],
-                                8 => vec![7],
-                                _ => panic!("weird fish"),
-                            };
-                            next
-                        })
-                        .collect();
-                    next
-                });
+        for _ in 0..n {
+            let old_fish = fishies.clone();
 
-                (t, end_result.len())
-            })
-            .collect();
-        println!("{:?}", m);
+            fishies[8] = old_fish[0];
+            fishies[7] = old_fish[8];
+            fishies[6] = old_fish[7] + old_fish[0];
+            fishies[5] = old_fish[6];
+            fishies[4] = old_fish[5];
+            fishies[3] = old_fish[4];
+            fishies[2] = old_fish[3];
+            fishies[1] = old_fish[2];
+            fishies[0] = old_fish[1];
+        }
 
-        let q: usize = fish.iter().map(|f| m.get(f).unwrap()).sum();
-
-        println!("{}", q);
+        println!("{:?}", fishies);
+        println!("{}", fishies.iter().sum::<u64>());
     }
 }
 
