6d922d131f87ac3882561c1357edbc9953e76e08
John Maguire <john.maguire@gmail.com>
2021-12-13 02:30:46 +0000

    Day 7 part 1

---
+++ day7/main.rs
@@ -0,0 +1,29 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() {
+    if let Ok(lines) = read_lines("./day7/input.txt") {
+        let line = lines.last().unwrap().unwrap();
+        let mut crabs: Vec<i32> = line.split(",").map(|x| x.parse().unwrap()).collect();
+
+        // Find the median and then move all crabs towards it?
+        crabs.sort();
+        println!("{:?}", crabs);
+        let median = crabs[crabs.len() / 2];
+        println!("Aligning to {}", median);
+
+        let res: i32 = crabs.iter().map(|x| (x - median).abs()).sum();
+        println!("{}", res);
+    }
+}
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
