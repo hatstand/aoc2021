84a949b35db531114d4749eacf76a9275631e8f3
John Maguire <john.maguire@gmail.com>
2021-12-22 02:13:44 +0000

    day16 - add sum

--- day16/main.rs
+++ day16/main.rs
@@ -68,6 +68,7 @@ fn literal(input: (&[u8], usize)) -> IResult<(&[u8], usize), Packet> {
 }
 
 fn num_subpackets(input: (&[u8], usize)) -> IResult<(&[u8], usize), u8> {
+    // bug? take(11size) causes a panic whereas take(8usize) then take(3usize) works.
     // take(8usize)(input)
 
     map(
@@ -111,14 +112,29 @@ fn parse(input: &[u8]) -> IResult<&[u8], Packet> {
 
 // type InputType = (&[u8], usize);
 
+fn sum_packet(p: &Packet) -> i32 {
+    match p {
+        Packet::Literal(lit) => lit.version as i32,
+        Packet::Operator(op) => {
+            // op.version as i32
+            op.subpackets
+                .iter()
+                .fold(op.version as i32, |acc, x| acc + sum_packet(x))
+        }
+    }
+}
+
 fn main() {
     if let Ok(lines) = read_lines("./day16/example.txt") {
         // let input = &[0xd2, 0xfe, 0x28];
-        let input = &[0xEE, 0x00, 0xD4, 0x0C, 0x82, 0x30, 0x60];
+        // let input = &[0xEE, 0x00, 0xD4, 0x0C, 0x82, 0x30, 0x60];
+        let input = &[0x8A, 0x00, 0x4A, 0x80, 0x1A, 0x80, 0x02, 0xF4, 0x78];
         let (left, packet) = parse(input).unwrap();
         // let (_, packet) = parse_packet(input).unwrap();
         println!("{:?}", packet);
         println!("{:?}", left);
+
+        println!("{:?}", sum_packet(&packet));
     }
 }
 
